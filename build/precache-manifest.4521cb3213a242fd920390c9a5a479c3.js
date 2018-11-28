9f9935f9066896d172b6dd3d541af509de9ca6ab
brdrummer <brdrummer@hotmail.com>
2018-11-28 16:40:52 -0600

    Heroku Deployment

---
+++ build/precache-manifest.4521cb3213a242fd920390c9a5a479c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d33c1e0f2e38ee1e4f98",
+    "url": "/static/js/main.d33c1e0f.chunk.js"
+  },
+  {
+    "revision": "94d8dcdcb75c4f487da0",
+    "url": "/static/js/1.94d8dcdc.chunk.js"
+  },
+  {
+    "revision": "94d8dcdcb75c4f487da0",
+    "url": "/static/css/1.c42a2e9a.chunk.css"
+  },
+  {
+    "revision": "7197d6f6775ea4a50dcfd8fd65993d38",
+    "url": "/index.html"
+  }
+];
